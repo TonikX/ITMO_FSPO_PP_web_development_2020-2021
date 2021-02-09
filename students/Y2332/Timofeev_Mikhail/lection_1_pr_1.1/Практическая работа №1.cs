a8f9ff563b03f9add7d431c83ff910be4b4d9ed5
Postograf <adventure686@mail.ru>
2021-02-09 11:22:08 +0300

    ИТМО ФСПО Y2332 Практическая работа №1 Тимофеев Михаил

---
+++ students/Y2332/Timofeev_Mikhail/lection_1_pr_1.1/Практическая работа №1.cs
@@ -0,0 +1,3 @@
+Frontend - UI/UX Пример: React, Angular, Vuejs в принципе похожи
+Backend - сервер Пример: Laravel, CakePHP, Django разные языки программирования
+(C# лучший язык программирования с ASP.NET и может быть Razor)
