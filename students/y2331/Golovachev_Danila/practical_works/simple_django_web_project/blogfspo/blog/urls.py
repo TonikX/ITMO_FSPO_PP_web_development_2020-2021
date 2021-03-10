232f3fb214ace3ec0d5e9d4f8e8b506401b240f8
DanilaG <danila.golovachev@mail.ru>
2021-03-10 21:36:04 +0300

    simple_django_web_project_2

--- students/y2331/Golovachev_Danila/practical_works/simple_django_web_project/blogfspo/blog/urls.py
+++ students/y2331/Golovachev_Danila/practical_works/simple_django_web_project/blogfspo/blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('<int:id>', detail, name='detail'),
+    path('Owner/', detail, name='detail'),
+    path('Cars/', list_car.as_view()),
+    path('Car/<int:pk>/', detail_car.as_view()),
 ]
--- students/y2331/Golovachev_Danila/practical_works/simple_django_web_project/blogfspo/blog/views.py
+++ students/y2331/Golovachev_Danila/practical_works/simple_django_web_project/blogfspo/blog/views.py
@@ -3,14 +3,27 @@ from django.http import Http404, HttpResponse
 from django.shortcuts import \
     render  # импортирует метод, который "запускает" созданную хтмл страницу и передает в нее указанные параметры
 from .models import *  # импортирует таблицу Poll из модели данных models, где polls - название приложения (и папки)
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 
-def detail(request, id):
+def detail(request):
     try:  # метод try-except - обработчик исключений
-        p = CarOwner.objects.get(pk=id)
+        p = CarOwner.objects.filter(pk__in=[1, 2, 3])
+        print(p)
     except CarOwner.DoesNotExist:
         raise Http404(
             "Poll does not exist")  # исключение которое будет вызвано, если блок try вернет значение False (не будут найдены записи в таблице Poll)
 
     return render(request, 'blogfspo/detail.html', {
         'owner': p})
+
+
+class list_car(ListView):
+    model = Car
+    template_name = 'blogfspo/detail_car.html'
+
+
+class detail_car(DetailView):
+    model = Car
+    template_name = 'blogfspo/detail_car_o.html'
