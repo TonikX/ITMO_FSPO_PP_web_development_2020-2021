6e457ecfab2b404f10f5f1d633b05508b38251d7
MaxDrift <maxlog58@gmail.com>
2021-03-23 00:37:12 +0300

    Completed 1.3

--- Students/y2332/Lozhkin_Maksim/practical_works/pr2/django_project_Lozhkin/project_first_app/urls.py
+++ Students/y2332/Lozhkin_Maksim/practical_works/pr2/django_project_Lozhkin/project_first_app/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('owner/<int:owner_id>/', detail)
+    path('owner/<int:owner_id>/', detail),
+    path('owner/list', owners_list),
+    path('cars/list', CarsList.as_view()),
+    path('car/<int:pk>', CarDetail.as_view()),
+    path('car/<int:pk>/update', UpdateCar.as_view()),
+    path('owner/create', create_owner),
+    path('car/create', CreateCar.as_view()),
+    path('car/<int:pk>/delete', DeleteCar.as_view())
 ]
--- Students/y2332/Lozhkin_Maksim/practical_works/pr2/django_project_Lozhkin/project_first_app/views.py
+++ Students/y2332/Lozhkin_Maksim/practical_works/pr2/django_project_Lozhkin/project_first_app/views.py
@@ -1,6 +1,10 @@
 from django.http import Http404
 from django.shortcuts import render
-from .models import CarOwner
+from .models import CarOwner, Car
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from .forms import OwnerForm
 
 
 def detail(request, owner_id):
@@ -11,3 +15,45 @@ def detail(request, owner_id):
     return render(request, 'owner.html', {'owner': owner})
 
 
+def owners_list(request):
+    context = {"dataset": CarOwner.objects.all()}
+    return render(request, "owners_list.html", context)
+
+
+class CarsList(ListView):
+    model = Car
+    template_name = 'cars_list.html'
+
+
+class CarDetail(DetailView):
+    model = Car
+    template_name = 'car_detail.html'
+
+
+class UpdateCar(UpdateView):
+    model = Car
+    template_name = 'car_update.html'
+    fields = ['number', 'brand', 'model', 'color']
+    success_url = '/cars/list'
+
+
+def create_owner(request):
+    context = {}
+    form = OwnerForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    context['form'] = form
+    return render(request, "create_owner.html", context)
+
+
+class CreateCar(CreateView):
+    model = Car
+    template_name = 'car_create.html'
+    fields = ['number', 'brand', 'model', 'color']
+    success_url = '/cars/list'
+
+
+class DeleteCar(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
+    success_url = '/cars/list'
