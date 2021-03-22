6f36b42abfcb107ac7f9dab8d78db775fed11f53
unknown <xiusha.alexeewa@yandex.ru>
2021-03-23 00:59:43 +0300

    ИТМО ФСПО Практическая работа №1.3 Алексеева О.Ю.

--- students/Y2337/Alekseeva_Oksana/practical_works/django_project_alekseeva/project_first_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Car, Car_owner, Ownership, Driving_license
-# Register your models here.
-
-admin.site.register(Car)
-admin.site.register(Car_owner)
-admin.site.register(Ownership)
-admin.site.register(Driving_license)
