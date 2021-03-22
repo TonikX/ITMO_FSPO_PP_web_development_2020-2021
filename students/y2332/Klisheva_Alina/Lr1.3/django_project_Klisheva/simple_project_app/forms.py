1e81b21e6593c2324af1e1b66c3b4ea6d8c2d4c1
Alina Kl <klishevaad@gmail.com>
2021-03-22 22:49:05 +0300

    Lr 1.3 added

---
+++ students/y2332/Klisheva_Alina/Lr1.3/django_project_Klisheva/simple_project_app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Car_owner
+
+
+
+class CarOwnerForm(forms.ModelForm):
+    class Meta:
+        model = Car_owner
+        fields = [
+            "first_name",
+            "last_name",
+            "birthdate",
+        ]
