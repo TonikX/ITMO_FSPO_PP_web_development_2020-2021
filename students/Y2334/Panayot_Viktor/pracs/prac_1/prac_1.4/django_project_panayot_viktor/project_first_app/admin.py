25eef660549b76259d61af669cc35574c709d195
Vitoliot <vpanaet@outlook.com>
2021-03-23 12:56:32 +0300

    prac_1.4

---
+++ students/Y2334/Panayot_Viktor/pracs/prac_1/prac_1.4/django_project_panayot_viktor/project_first_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(auto)
+admin.site.register(CarOwner)
+admin.site.register(CarOwnership)
+admin.site.register(driver_license)
