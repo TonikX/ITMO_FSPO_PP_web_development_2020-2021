20aad517948eb1544cc3e8516eb6857c4ae6f147
Lisov_Nikita <niklis60003@gmail.com>
2021-04-14 12:04:29 +0300

    Lisov Nikita Y2336 PP pr2

--- students/Y2336/Lisov_Nikita/plactical_works/simple_django_web_project/project_first_app/models.py
+++ students/Y2336/Lisov_Nikita/plactical_works/simple_django_web_project/project_first_app/models.py
@@ -6,6 +6,7 @@ class Owner(models.Model):
     last_name = models.CharField(max_length=30)
     birth_date = models.DateTimeField(null=True)
 
+
 class License(models.Model):
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
     num = models.CharField(max_length=10)
@@ -19,7 +20,29 @@ class Auto(models.Model):
     color = models.CharField(max_length=30, null=True)
 
 class Ownership(models.Model):
-    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
-    auto = models.ForeignKey(Auto, on_delete=models.CASCADE)
+    owners = models.ManyToManyField(Owner)
+    autos = models.ManyToManyField(Auto)
     date_start = models.DateTimeField()
     date_end = models.DateTimeField(null=True)
+
+class ExampleModel(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+    def __str__(self):
+        return self.title
+
+class Publisher(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    birthdate = models.DateField()
+
+    def __str__(self):
+	       return "{} {}".format(self.first_name, self.last_name)
+
+class Book(models.Model):
+  name = models.CharField(max_length=100)
+  desc = models.CharField(max_length=200)
+  publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+  def __str__(self):
+      return "{}, {}".format(self.name, self.publisher)
--- students/Y2336/Lisov_Nikita/plactical_works/simple_django_web_project/project_first_app/urls.py
+++ students/Y2336/Lisov_Nikita/plactical_works/simple_django_web_project/project_first_app/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
-from . import views #подключение файла контроллеров,описанного в пункте 3
+from . import views
+from .views import example_view
+from .views import list_view
+from .views import ExampleList
+from .views import *
+
 urlpatterns = [
-    path('owner/1', views.detail), #пример вызова контроллера (функции) с именем "special_case_200" из файда views
-    path('owner/<int:owner_id>/', views.detail), #пример вызова контроллера (функции) с именем "year_archive" из файда views и передачи в него переменной "year"
+    path('owners/', views.detail), #пример вызова контроллера (функции) с именем "special_case_200" из файда views
+    path('autos/', AllAutos.as_view()), #пример вызова контроллера (функции) с именем "special_case_200" из файда views
+    path('auto/<int:pk>/', AllAuto.as_view()), #пример вызова контроллера (функции) с именем "year_archive" из файда views и передачи в него переменной "year"
+    path('time/', example_view),
+    path('example_list/', list_view),
+    path('сvb_example/', ExampleList.as_view()),
+    path('publisher/<int:pk>/', PublisherRetrieveView.as_view()),
+    path('book/list/', BookListView.as_view()),
 ]
