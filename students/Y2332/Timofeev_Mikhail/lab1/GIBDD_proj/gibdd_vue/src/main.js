8cc0503a5665dd798ec6e246d0ca146abd71c5c0
Postograf <adventure686@mail.ru>
2021-05-28 19:02:45 +0300

    Create vue2 proj add vuetify

--- students/Y2332/Timofeev_Mikhail/lab1/GIBDD_proj/gibdd_vue/src/main.js
+++ students/Y2332/Timofeev_Mikhail/lab1/GIBDD_proj/gibdd_vue/src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import router from './router'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  router,
+  render: h => h(App)
+}).$mount('#app')
