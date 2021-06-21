ada5c7b4b17cc86e2128ed8a1683292652112439
RamanPan <ramanpan123@gmail.com>
2021-06-21 21:43:45 +0300

    Practic 1.4 Not to lategit add *!

---
+++ students/Y2334/RamanPan/1_4 praktos/blog/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+# from .models import autoOwner
+from .models import User
+  
+  
+# creating a form
+class ExampleForm(forms.ModelForm):
+  
+    # create meta class
+    class Meta:
+        # specify model to be used
+        model = User
+  
+        # specify fields to be used
+        fields = [
+            # "last_name",
+            # "first_name",
+            "username",
+            "birth_date",
+            "passport",
+            "address",
+            "nationality"
+        ]
